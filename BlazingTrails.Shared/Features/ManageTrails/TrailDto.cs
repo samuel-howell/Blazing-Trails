b3f308751d076de63d2ab64613a1c4a4f0a78f7e
User <User@DESKTOP-OFNSVN2>
2022-12-14 12:13:14 -0500

    added validation to addTrail form. start on section 5.3

--- BlazingTrails.Shared/Features/ManageTrails/TrailDto.cs
+++ BlazingTrails.Shared/Features/ManageTrails/TrailDto.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FluentValidation;
 
 namespace BlazingTrails.Shared.Features.ManageTrails
 {
@@ -20,6 +21,32 @@ namespace BlazingTrails.Shared.Features.ManageTrails
         {
             public int Stage { get; set; }
             public string Description { get; set; } = "";
+        }
+    }
+
+    // using Fluent Validation
+    public class TrailValidator : AbstractValidator<TrailDto> // Validation classes must inherit from the AbstractValidator<T> class, T being the class to be validated
+    {
+        public TrailValidator()  // Validation rules are defined in the constructor of the validation class
+        {
+            // Validation rules are defined using a fluent syntax, hence the name. Each rule defines the property it’s for, the criteria, and the error message to show if the criteria isn’t met
+
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Please enter a name.");
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Please enter a description.");
+            RuleFor(x => x.Location).NotEmpty().WithMessage("Please enter a location.");
+            RuleFor(x => x.Length).GreaterThan(0).WithMessage("Please enter a length.");
+            RuleFor(x => x.Route).NotEmpty().WithMessage("Please add a route instruction.");
+
+            RuleForEach(x => x.Route).SetValidator(new RouteInstructionValidator()); // wire up the RouteInstructionValidator to the Trail Validator
+        }
+    }
+
+    public class RouteInstructionValidator : AbstractValidator<TrailDto.RouteInstruction>
+    {
+        public RouteInstructionValidator()
+        {
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Please enter a description.");
+            RuleFor(x => x.Stage).NotEmpty().WithMessage("Please enter a stage.");
         }
     }
 }
